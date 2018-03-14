820cc9601dd5e06c8ca640d0397c5581f6d42790
Komang Eric Widhi Antara <nukenin.konoha@gmail.com>
2018-03-14 17:02:52 +0800

    Add files via upload

--- App.js
+++ App.js
@@ -1,54 +1,25 @@
-import React, { Component } from 'react';
-import { AppRegistry, SectionList, StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  AppRegistry,
+  StyleSheet,
+  View
+} from 'react-native';
+import Sales from './src/MainApp';
 
-export default class Listview extends Component {
+export default class App extends React.Component {
   render() {
     return (
-      <View style={styles.container}>
-      <Text style={{ padding: 4, fontSize: 22, color: 'blue', textAlign: 'center' }} >
-           {'\n'}
-            List View Nama nama buah
-      </Text>
-        <SectionList
-          sections={[
-            {title: 'A', data: ['Apple','Avocado']},
-            {title: 'B', data: ['Banana', 'Blueberry']},
-            {title: 'C', data: ['Cherry', 'Coconut']},
-            {title: 'D', data: ['Durian', 'Dragon Fruit']},
-          ]}
-          renderItem={({item}) => <Text style={styles.item}>{item}</Text>}
-          renderSectionHeader={({section}) => <Text style={styles.sectionHeader}>{section.title}</Text>}
-          keyExtractor={(item, index) => index}
-        />
-        <Text style={{ padding: 4, fontSize: 22, color: 'blue', textAlign: 'center' }} >
-           {'\n'}
-            Eric Widhi Antara
-        </Text>
+      <View style={styles.containerMain}>
+        <Sales/>
       </View>
     );
   }
 }
 
 const styles = StyleSheet.create({
-  container: {
-   flex: 1,
-   paddingTop: 22
-  },
-  sectionHeader: {
-    paddingTop: 2,
-    paddingLeft: 10,
-    paddingRight: 10,
-    paddingBottom: 2,
-    fontSize: 14,
-    fontWeight: 'bold',
-    backgroundColor: 'rgba(247,247,247,1.0)',
-  },
-  item: {
-    padding: 10,
-    fontSize: 18,
-    height: 44,
-  },
+  containerMain: {
+    paddingTop: 20,
+    flex: 1,
+    backgroundColor: '#E8EAF6'
+  }
 })
-
-// skip this line if using Create React Native App
-AppRegistry.registerComponent('AwesomeProject', () => Listview);
